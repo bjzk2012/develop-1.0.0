3fd49e3ef4e6217572451b95f423334255845dc2
张奎 <bjzk_2012_zk@163.com>
2019-07-15 14:19:43 +0800

    调整了整体的依赖版本，解决了大部分依赖版本冲突

--- develop.orm/develop.orm.es/src/main/java/cn/kcyf/tools/es/config/EsConfig.java
+++
@@ -1,7 +0,0 @@
-package cn.kcyf.tools.es.config;
-
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-@EnableElasticsearchRepositories(basePackages = "cn.kcyf.tools.es.repository")
-public class EsConfig {
-}
