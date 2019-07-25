e1f06e1a8a8d2bcf3ea1d3de0ee6c6f2922e51d7
张奎 <bjzk_2012_zk@163.com>
2019-07-25 22:41:52 +0800

    调整JPA

--- develop.orm/develop.orm.jpa/src/main/java/cn/kcyf/orm/jpa/entity/TableDomain.java
+++ develop.orm/develop.orm.jpa/src/main/java/cn/kcyf/orm/jpa/entity/TableDomain.java
@@ -24,33 +24,39 @@ public abstract class TableDomain extends IdDomain {
      * 创建人用户编号
      */
     @Column(name = "create_user_id", nullable = false, updatable = false)
+    @JSONField(serialize = false, deserialize = false)
     protected Long createUserId;
     /**
      * 创建人用户名称
      */
     @Column(name = "create_user_name", nullable = false, updatable = false)
+    @JSONField(serialize = false, deserialize = false)
     protected String createUserName;
     /**
      * 最后更新时间
      */
     @Column(name = "last_update_time", insertable = false)
+    @JSONField(serialize = false, deserialize = false)
     @Temporal(TemporalType.TIMESTAMP)
     protected Date lastUpdateTime;
     /**
      * 最后更新用户编号
      */
     @Column(name = "last_update_user_id", insertable = false)
+    @JSONField(serialize = false, deserialize = false)
     protected Long lastUpdateUserId;
     /**
      * 最后更新用户名称
      */
     @Column(name = "last_update_user_name", insertable = false)
+    @JSONField(serialize = false, deserialize = false)
     protected String lastUpdateUserName;
     /**
      * 版本号,用于实现乐观锁
      */
     @Version
     @Column(name = "version", nullable = false)
+    @JSONField(serialize = false, deserialize = false)
     protected int version;
 
     @PrePersist
